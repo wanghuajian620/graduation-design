4ad8b42183836d8418e490d7c2b0d4c2e5e4a611
wanghuajian620 <154659041@qq.com>
2018-05-30 22:08:53 +0800

    加入学生选题系统的组件

--- management/src/libs/request.js
+++ management/src/libs/request.js
@@ -3,7 +3,7 @@ import wepy from 'wepy'
 export const RepoList = async () => {
   try {
     const repo = await wepy.request({
-      url: '',
+      url: 'http://192.168.0.109:8080/graduation/student/get',
       method: 'GET'
     })
     return repo.data.data
