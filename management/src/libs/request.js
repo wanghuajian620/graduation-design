449995401a67b407d8df78599e2408d579d755ea
wanghuajian620 <154659041@qq.com>
2018-06-02 16:53:54 +0800

    学生 可以浏览 选题

--- management/src/libs/request.js
+++ management/src/libs/request.js
@@ -1,13 +1,20 @@
-import wepy from 'wepy'
+// import wepy from 'wepy'
 
-export const RepoList = async () => {
-  try {
-    const repo = await wepy.request({
-      url: 'http://192.168.0.109:8080/graduation/student/get',
-      method: 'GET'
-    })
-    return repo.data.data
-  } catch (err) {
-    console.error(`fetch repos error: ${JSON.stringify(err)}`)
-  }
-}
+// var header
+// header = {
+//   'content-type': 'application/x-www-form-urlencoded',
+//   'cookie': wepy.getStorageSync('sessionid') // 读取cookie
+// }
+// export const RepoList = async () => {
+//   try {
+//     const repo = await wepy.request({
+//       url: 'http://192.168.0.105:8080/graduation/student/get',
+//       method: 'GET',
+//       header: header
+//     })
+//     console.log(repo.data, 'rrrrr')
+//     return repo.data.data
+//   } catch (err) {
+//     console.error(`fetch repos error: ${JSON.stringify(err)}`)
+//   }
+// }
